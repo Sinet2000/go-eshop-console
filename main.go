478127293b216caafdb44a25db66f6e5690dd78e
Nikita Nikitins <59967050+Sinet2000@users.noreply.github.com>
2025-01-12 21:58:15 +0200

    WSC-1_1: Configure Docker for MongoDB and Application Build #11 (#12)
    * #WSC-1_1: Configure Docker for MongoDB and Application Build #11

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 func main() {
-	fmt.Println("Hello from WSC")
+	// Load environment variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Get MongoDB configuration from environment variables
+	mongoURI := os.Getenv("MONGO_URI")
+	dbName := os.Getenv("MONGO_DB_NAME")
+
+	// Connect to MongoDB
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(mongoURI).SetServerAPIOptions(serverAPI)
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		log.Fatalf("Failed to create MongoDB client: %v", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatalf("Failed to connect to MongoDB: %v", err)
+	}
+	defer client.Disconnect(ctx)
+
+	// Test the connection
+	db := client.Database(dbName)
+	fmt.Printf("Connected to MongoDB database: %s\n", db.Name())
 }

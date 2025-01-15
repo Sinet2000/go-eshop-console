413d3eb59d8affc739b72a14468eb98ab39aaea7
Sinet2000 <giinger11352@gmail.com>
2025-01-15 11:20:54 +0200

    #WSC-3_2: Improved design, RBA, handlers

--- config/config.go
+++ config/config.go
@@ -9,18 +9,18 @@ import (
 )
 
 func LoadConfig() {
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 }
 
 func GetEnv(key string) string {
-    value := os.Getenv(key)
-    if value == "" {
-        log.Fatalf("Environment variable %s not set", key)
-    }
-    return value
+	value := os.Getenv(key)
+	if value == "" {
+		log.Fatalf("Environment variable %s not set", key)
+	}
+	return value
 }
 
 func GetProjectRoot() string {
